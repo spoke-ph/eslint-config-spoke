dd0538113f0e510a9b7928c0e887d9f55cdb6de9
linlin <54916735+linlin1023@users.noreply.github.com>
2022-06-02 11:02:51 +1200

    Add curly rule for statement after flow control clause (#13)
    * Add curly rule for statement after flow control clause
    
    * move deps
    
    * Add indent rule for typescript
    
    * fix indentation in typescript.js
    
    Co-authored-by: Jenny <jenny.lee@spokephone.com>

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,71 +1,78 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "project": "tsconfig.json",
-      "sourceType": "module"
-  },
-  "plugins": [
-      "@typescript-eslint",
-  ],
-  "rules": {
-      "@typescript-eslint/naming-convention": ["error",
-        {
-          selector: "default",
-          format: ["camelCase"],
-          leadingUnderscore: "allow"
-        },
-        {
-          selector: "variable",
-          format: ["camelCase", "UPPER_CASE", "PascalCase"],
-          leadingUnderscore: "allow"
-        },
-        {
-          selector: "function",
-          format: ["camelCase"],
-          leadingUnderscore: "allow"
-        },
-        {
-          selector: ["typeLike", "typeAlias", "typeParameter"],
-          format: ["PascalCase"],
-          leadingUnderscore: "allow"
-        },
-        {
-          selector: "enumMember",
-          format: ["PascalCase", "camelCase"]
-        },
-        {
-          selector: ["typeProperty", "typeAlias", "typeParameter"],
-          format: ["PascalCase", "camelCase"],
-          leadingUnderscore: "allow"
-        },
-        {
-          selector: ["objectLiteralProperty", "objectLiteralMethod"],
-          format: ["camelCase", "UPPER_CASE", "PascalCase", "snake_case"] // snake_case is for some external APIs and libraries
-        }
-      ],
-      "@typescript-eslint/member-delimiter-style": [
-          "error",
-          {
-              "multiline": {
-                  "delimiter": "semi",
-                  "requireLast": true
-              },
-              "singleline": {
-                  "delimiter": "semi",
-                  "requireLast": false
-              }
-          }
-      ],
-      "@typescript-eslint/prefer-namespace-keyword": "error",
-      "@typescript-eslint/no-unused-vars": ["error"],
-      "@typescript-eslint/semi": [
-          "error",
-          "always"
-      ],
-      "@typescript-eslint/space-within-parens": [
-          "off",
-          "never"
-      ],
-      "@typescript-eslint/type-annotation-spacing": "error"
-  }
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+    ],
+    "rules": {
+        "@typescript-eslint/naming-convention": ["error",
+            {
+                selector: "default",
+                format: ["camelCase"],
+                leadingUnderscore: "allow"
+            },
+            {
+                selector: "variable",
+                format: ["camelCase", "UPPER_CASE", "PascalCase"],
+                leadingUnderscore: "allow"
+            },
+            {
+                selector: "function",
+                format: ["camelCase"],
+                leadingUnderscore: "allow"
+            },
+            {
+                selector: ["typeLike", "typeAlias", "typeParameter"],
+                format: ["PascalCase"],
+                leadingUnderscore: "allow"
+            },
+            {
+                selector: "enumMember",
+                format: ["PascalCase", "camelCase"]
+            },
+            {
+                selector: ["typeProperty", "typeAlias", "typeParameter"],
+                format: ["PascalCase", "camelCase"],
+                leadingUnderscore: "allow"
+            },
+            {
+                selector: ["objectLiteralProperty", "objectLiteralMethod"],
+                format: ["camelCase", "UPPER_CASE", "PascalCase", "snake_case"] // snake_case is for some external APIs and libraries
+            }
+        ],
+        "@typescript-eslint/member-delimiter-style": [
+            "error",
+            {
+                "multiline": {
+                    "delimiter": "semi",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/no-unused-vars": ["error"],
+        "@typescript-eslint/semi": [
+            "error",
+            "always"
+        ],
+        "@typescript-eslint/space-within-parens": [
+            "off",
+            "never"
+        ],
+        "@typescript-eslint/type-annotation-spacing": "error",
+        "@typescript-eslint/indent": [
+            "error",
+            2,
+            {
+                SwitchCase: 1
+            }
+        ],
+    }
 };
